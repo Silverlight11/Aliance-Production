25e47f8708bf33159a199652a599d178d748b321
Silverlight11 <silver110194@yandex.ru>
2023-09-14 20:27:34 +0300

    fix: js3

--- js/main.js
+++ js/main.js
@@ -2,26 +2,31 @@ const navbar = document.querySelector('.navbar')
 const logo = document.querySelector('.logo-svg use')
 const mMenuToggle = document.querySelector('.mobile-menu-toggle')
 const menu = document.querySelector('.mobile-menu')
+
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light")
   logo.href.baseVal = "img/sprite.svg#logo-dark"
 }
+
 const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light")
   logo.href.baseVal = "img/sprite.svg#logo-light" 
 }
+
 const openMenu = (event) => {   // функция открывания меня 
   menu.classList.add('is-open'); // вешает класс "is open"
   mMenuToggle.classList.add('close-menu');
   document.body.style.overflow = "hidden"; // запрещаем прокрутку сайта под меню
   lightModeOn();
 }
+
 const closeMenu = (event) => {   // функция закрытия меня 
   menu.classList.remove('is-open'); // убирает класс "is open"
   mMenuToggle.classList.remove('close-menu');
   document.body.style.overflow = ""; // возвращаем прокрутку сайта под меню
   lightModeOff();
 }
+
 window.addEventListener('scroll', () => {
   this.scrollY > 1 ? lightModeOn() : lightModeOff();
 })
